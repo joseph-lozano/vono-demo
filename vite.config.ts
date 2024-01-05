5e5714db872890cd96762f06aedcf752f8bd29c6
Joseph Lozano <me@lozanojoseph.com>
2024-01-05 15:11:45 -0500

    🚀 Cloudflare adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import vono from "@gaiiaa/vono";
+import { cloudflare } from "@gaiiaa/vono/adaptors";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vono()],
+  plugins: [react(), vono({ adaptor: cloudflare() })],
 });
